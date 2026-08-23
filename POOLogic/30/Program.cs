5148957c368c13fb7303fb5f1b5cb8160060482e
Najabi Salgado <najabisalgadogiraldo@gmail.com>
2026-08-23 12:13:48 -0500

    Work on MatrixOperations - not finished

---
+++ POOLogic/30/Program.cs
@@ -0,0 +1,46 @@
+﻿using Utilities_Shared;
+
+var answer = String.Empty;
+var options = new List<string> { "s", "n" };
+var sumatoria = 0;
+
+
+var order = ConsoleExtensions.GetInt("Ingrese el order de la matriz: ");
+int[,] matriz = new int[order, order];
+
+do
+{
+
+
+    for (int f = 0; f < order; f++) // filas
+    {
+        for (int c = 0; c < order; c++) // columnas. Recorrido por filas
+        {
+            matriz [f, c] = (f + 1) - c;
+            Console.Write($"{matriz[f, c]}  ");
+        }
+
+        Console.Write("\n");
+
+    }
+
+    for (int f = 0; f < order; f++) 
+    {
+        for (int c = 0; c < order; c++) {
+
+
+
+        }
+
+    }
+
+    Console.WriteLine($"La sumatoria es: {sumatoria}");
+
+
+
+    do
+    {
+        answer = ConsoleExtensions.GetValidOptions("Deseas continuar (S)í, (N)o?", options);
+
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
